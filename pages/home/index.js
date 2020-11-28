b608cc17553a0a156a22167dd059469bb2595dda
Roxy Hancke <roxy.hancke@gmail.com>
2020-11-28 16:16:23 +0800

    tabbar 2

--- pages/home/index.js
+++ pages/home/index.js
@@ -7,30 +7,43 @@ Page({
       id:"",
       image1:""
     }], 
-
+    userinfos: [{
+      id:"",
+      display_name:""
+    }]
   },
 
 onLoad: function () {
   const Stories = new wx.BaaS.TableObject("ac_stories");
+  const UserInfos = new wx.BaaS.TableObject("ac_userInfo");
 
-  Stories.find().then((res) => {
+  Stories.expand(['userID']).find().then((res) => {
     console.log("detail page result", res);
     this.setData({
       stories:res.data.objects,
     });
+  
+
   }, (error) => {
     console.log("error", error);
   }
   );
+  
 
   },
 
 
 
-toStories: function () {
+toNavigate: function () {
   wx.switchTab({
     url: 'pages/profile/profile',
   })
   },
+
+toStories: function (e) {
+  wx.navigateTo({
+    url: `/pages/stories/index?id=${e.currentTarget.id}`,
+  });
+}
 
 })
