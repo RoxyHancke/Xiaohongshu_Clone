32d943f8447a9cb7197889d1abe74a5fc2387510
nefishastemir <nefise1203>
2020-11-28 14:21:18 +0800

    Stories Details

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 //app.js
 
-
+const app = getApp();
 App({
   onLaunch: function() {
     wx.BaaS = requirePlugin('sdkPlugin')
     //让插件帮助完成登录、支付等功能
     wx.BaaS.wxExtend(wx.login, wx.getUserInfo, wx.requestPayment)
-
     let clientID = 'c28c8ae0cae5f3185379'  // 应用名称: TheOutdoorCompany
     wx.BaaS.init(clientID)
   },
